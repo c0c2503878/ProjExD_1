292dd152b37c0ddb9b96d18985b926cef86bf18e
c0c2503878 <c0c2503878@edu.teu.ac.jp>
2026-06-09 15:52:53 +0900

    練習5と練習6:背景画像が右から左に流れるようになった

--- pygame_image.py
+++ pygame_image.py
@@ -10,18 +10,19 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kk_img = pg.image.load("fig/3.jpg")
+    kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img,300,200)
+        x = tmr
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
