b5f1c9539fe87fbf2261e01cc72c9b94361bb6f9
c0c2503878 <c0c2503878@edu.teu.ac.jp>
2026-06-09 17:12:58 +0900

    演習1:何もキーを押してないとき左、右を押してるとき右に進む

--- pygame_image.py
+++ pygame_image.py
@@ -25,10 +25,10 @@ def main():
             kk_rct.move_ip(0, -1)
         if key_lst[pg.K_DOWN]:
             kk_rct.move_ip(0, +1)
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
         if key_lst[pg.K_RIGHT]:
             kk_rct.move_ip(+1, 0)
+        else:
+            kk_rct.move_ip(-1,0)
         x = tmr%3200
         screen.blit(bg_img, [-x, 0])
         screen.blit(bg_img2, [-x+1600, 0])
